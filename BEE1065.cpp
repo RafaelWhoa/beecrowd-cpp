f7da28ae5667b6cc6ef0ebe9025f5c9cee8ab41b
Rafael Costa <44804@tjmt.jus.br>
2024-04-27 21:38:19 -0400

    BEE1065

--- BEE1065.cpp
+++ BEE1065.cpp
@@ -1,3 +1,20 @@
 //
 // Created by Rafael Costa on 27/04/24.
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int numbers[5];
+    int even_count = 0;
+
+    for (int & number : numbers) {
+        cin >> number;
+        if (number % 2 == 0) {
+            even_count++;
+        }
+    }
+
+    cout << even_count << " valores pares" << endl;
+}
 //
